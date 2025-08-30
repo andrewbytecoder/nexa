602c10a2c403b1c3c626c898745e91163a797c32
wangyazhou <564631192@qq.com>
2025-08-30 18:35:11 +0800

    feat(net): 添加网络信息获取功能
    - 新增 pernic命令行参数，用于控制是否按网络接口显示信息
    - 实现网络信息获取和显示逻辑- 添加错误处理和日志记录

--- pkg/net/psutil/net.go
+++ pkg/net/psutil/net.go
@@ -3,6 +3,7 @@ package psutil
 import (
 	"github.com/shirou/gopsutil/v4/net"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap"
 	"strings"
 )
 
@@ -12,6 +13,7 @@ type PsNet struct {
 	// Command line flags.
 	readable bool
 	showType string
+	pernic   bool
 }
 
 func NewPsnet(psUtil *PsUtil) *PsNet {
@@ -23,17 +25,22 @@ func NewPsnet(psUtil *PsUtil) *PsNet {
 func (psnet *PsNet) ParseFlags(cmd *cobra.Command) {
 	cmd.Flags().BoolVarP(&psnet.readable, "human-readable", "H", true, "human readable output")
 	cmd.Flags().StringVarP(&psnet.showType, "type", "t", "all", strings.Join([]string{tAll}, "|"))
+	cmd.Flags().BoolVarP(&psnet.pernic, "pernic", "p", true, "If pernic argument is false, return only sum of all information")
 }
 
-func (psnet *PsNet) GetnetInfo() {
-	if psnet.showType == tAll || psnet.showType == "" {
+func (psNet *PsNet) GetnetInfo() {
+	if psNet.showType == tAll || psNet.showType == "" {
 	}
 
-	if psnet.showType == tAll || psnet.showType == "" {
+	if psNet.showType == tAll || psNet.showType == "" {
 	}
 }
 
-func (psnet *PsNet) shownetAvg() {
-
-	net.IOCounters(true)
+func (psNet *PsNet) shownetAvg() {
+	counters, err := net.IOCounters(psNet.pernic)
+	if err != nil {
+		psNet.psUtil.logger.Error("Error getting network info", zap.Error(err))
+		return
+	}
+	net.PrintIOCounter(counters)
 }
